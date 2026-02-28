5024fdfc7773386a49e7e82a719f4dd33764a5d8
Joshua Esplin <joshua@esplin.org>
2026-02-27 18:33:07 -0700

    db migrations

---
+++ PlantTracker.Api/Data/Migrations/20260228011641_AddPlantPhotos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlantTracker.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlantPhotos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlantPhotos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserPlantId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    ImageData = table.Column<string>(type: "TEXT", nullable: false),
+                    Caption = table.Column<string>(type: "TEXT", nullable: true),
+                    TakenAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlantPhotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlantPhotos_UserPlants_UserPlantId",
+                        column: x => x.UserPlantId,
+                        principalTable: "UserPlants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlantPhotos_UserPlantId",
+                table: "PlantPhotos",
+                column: "UserPlantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlantPhotos");
+        }
+    }
+}
